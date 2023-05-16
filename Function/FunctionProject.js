96e149e74770552b2432502ad3a42607d365b3e1
MonkeyZ-04 <89679927+MonkeyZ-04@users.noreply.github.com>
2023-05-16 23:29:30 +0700

    function

--- Function/FunctionProject.js
+++ Function/FunctionProject.js
@@ -1,21 +1,21 @@
-function toCalsius(fahrenheit)
+function toCelsius(fahrenheit)
 {
     // let fahrenheit = Number(prompt("Fahrenheit: "))
     let value = ((fahrenheit - 32)*5/9)
-    // document.getElementById("anwer_section").innerHTML = value.toFixed(2)+" Calsius";
-    return value.toFixed(2)+" Calsius";
+    // document.getElementById("anwer_section").innerHTML = value.toFixed(2)+" Celsius";
+    return value.toFixed(2)+" Celsius";
 }
 function display(elementId, value)
 {
     document.getElementById(elementId).innerHTML = value
 }
-function toFahrenheit(calsius){
+function toFahrenheit(celsius){
     // let calsius = Number(prompt("Calsius: "))
-    let value = (calsius*9/5)+32
+    let value = (celsius*9/5)+32
     // document.getElementById("toFahrenheit").innerHTML = value.toFixed(2)+" fahrenheit";
     return value.toFixed(2)+" fahrenheit"
 }
-alert(toCalsius(120))
-display("toCalsius",toCalsius(120))
+alert(toCelsius(120))
+display("toCalsius",toCelsius(120))
 alert(toFahrenheit(25))
 display("toFahrenheit", toFahrenheit(25))
--- Function/FunctionProjectFtoC.js
+++ Function/FunctionProjectFtoC.js
@@ -1,13 +1,13 @@
-function toCalsius()
+function toCelsius()
 {
     let fahrenheit = Number(prompt("Fahrenheit: "))
     let value = ((fahrenheit - 32)*5/9)
-    document.getElementById("anwer_section").innerHTML = value.toFixed(2)+" Calsius";
+    document.getElementById("anwer_section").innerHTML = value.toFixed(2)+" Celsius";
 }
 function toFahrenheit(){
-    let calsius = Number(prompt("Calsius: "))
+    let calsius = Number(prompt("Celsius: "))
     let value = (calsius*9/5)+32
     document.getElementById("anwer_section2").innerHTML = value.toFixed(2)+" fahrenheit";
 }
-toCalsius()
+toCelsius()
 toFahrenheit()
